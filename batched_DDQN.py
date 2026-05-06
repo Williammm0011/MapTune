41fecdc2268d1ff72f0e8513c0a1a7061610c364
Williammm0011 <williamsu0011@gmail.com>
2026-05-06 15:38:13 +0800

    update DDQN log

--- batched_DDQN.py
+++ batched_DDQN.py
@@ -214,7 +214,7 @@ def train_agent(num_episodes, agent, env, batch_size, buffer_size):
                 batch = random.sample(replay_buffer, batch_size)
                 agent.update_batch(batch)  # Process batch update
 
-        print(f"Episode {episode + 1}, Highest Reward = {highest_reward}")
+        print(f"Episode {episode + 1}, Delay = {delay}, Area = {area}, Reward = {reward}, Highest Reward = {highest_reward}")
         with open(".q_learning_progress", "w") as _pf:
             _pf.write(f"{episode + 1}/{num_episodes}")
 
